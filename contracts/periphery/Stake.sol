325c4b57cf4903276bd6f605df30f534e9bb583d
byterui <shirui_yj133@163.com>
2021-08-13 15:46:01 +0800

    update claim

--- contracts/periphery/Stake.sol
+++ contracts/periphery/Stake.sol
@@ -23,6 +23,7 @@ contract Stake is Pausable, ReentrancyGuard, AdminUpgradeable {
     uint256 public immutable START_BLOCK;
     uint256 public immutable END_BLOCK;
     uint256 private _total_insterst;
+    uint256 private _total_reward;
 
     mapping(address => StakeData) private StakeDatas;
 
@@ -37,56 +38,69 @@ contract Stake is Pausable, ReentrancyGuard, AdminUpgradeable {
         REWARD_TOKEN = reward_token;
         START_BLOCK = start_block;
         END_BLOCK = end_block;
+        _total_reward = IERC20(reward_token).balanceOf(address(this));
+    }
+
+    function syncReward() public {
+        require(block.number < END_BLOCK, "over stake period");
+        _total_reward = IERC20(REWARD_TOKEN).balanceOf(address(this));
     }
 
     function stake(uint256 amount) public returns (uint256 added_interest) {
         require(block.number < END_BLOCK, "over stake period");
         require(amount > 0, "cannot stake 0 amount");
 
-        Helper.safeTransferFrom(STAKED_TOKEN, msg.sender, address(this), amount);
-        
+        Helper.safeTransferFrom(
+            STAKED_TOKEN,
+            msg.sender,
+            address(this),
+            amount
+        );
+
         StakeData memory data = StakeDatas[msg.sender];
-        
-        data.blocknumner = data.blocknumner < START_BLOCK ? START_BLOCK : block.number;
+
+        data.blocknumner = data.blocknumner < START_BLOCK
+            ? START_BLOCK
+            : block.number;
 
         added_interest = amount.mul(END_BLOCK - data.blocknumner);
-        
+
         _total_insterst = _total_insterst.add(added_interest);
-        
+
         data.stake_amount = data.stake_amount.add(amount);
         data.interest = data.interest.add(added_interest);
         StakeDatas[msg.sender] = data;
     }
 
-    function redeem(uint256 amount) public returns (uint256 removed_interest){
+    function redeem(uint256 amount) public returns (uint256 removed_interest) {
         require(amount > 0, "cannot redeem 0 amount");
         require(block.number > START_BLOCK, "stake not start");
 
         StakeData memory data = StakeDatas[msg.sender];
         require(data.stake_amount <= amount, "insufficent stake amount");
-        
+
         data.blocknumner = block.number < END_BLOCK ? block.number : END_BLOCK;
-        
+
         removed_interest = amount.mul(END_BLOCK - data.blocknumner);
-        
+
         _total_insterst = _total_insterst.sub(removed_interest);
-        
+
         data.stake_amount = data.stake_amount.sub(amount);
         data.interest = data.interest.sub(removed_interest);
         StakeDatas[msg.sender] = data;
-        
+
         Helper.safeTransfer(STAKED_TOKEN, msg.sender, amount);
     }
-    
-    function claim() public returns(uint256 claim_reward_amount){
+
+    function claim() public returns (uint256 claim_reward_amount) {
         require(block.number > END_BLOCK, "in stake period");
-        
+
         StakeData memory data = StakeDatas[msg.sender];
         require(data.interest > 0, "no interest");
-        
-        uint256 total_reward = IERC20(REWARD_TOKEN).totalSupply();
 
-        claim_reward_amount = total_reward.mul(data.interest) / _total_insterst;
+        claim_reward_amount =
+            _total_reward.mul(data.interest) /
+            _total_insterst;
 
         data.interest = 0;
         data.blocknumner = block.number;
