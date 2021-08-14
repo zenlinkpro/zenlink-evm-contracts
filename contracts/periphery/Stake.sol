3c0d31b2827f5c30e083f054f4ece25fa7f9b2e4
Hayden Fan <haosong0323@gmail.com>
2021-08-15 00:28:10 +0800

    refactor: add event and remove package-lock (#5)
    * refactor: add event and remove package-lock
    
    * refactor: rename require error message
    
    * feat: add estimated rewards view
    
    * chore: add comments
    
    * feat: add blackList

--- contracts/periphery/Stake.sol
+++ contracts/periphery/Stake.sol
@@ -4,54 +4,85 @@ pragma solidity >=0.8.0;
 
 import "../libraries/Helper.sol";
 import "../libraries/Math.sol";
+import "../libraries/AdminUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "../libraries/AdminUpgradeable.sol";
 
 contract Stake is Pausable, ReentrancyGuard, AdminUpgradeable {
     using Math for uint256;
 
-    struct StakeData {
-        uint256 stake_amount;
-        uint256 block_number;
-        uint256 interest;
+    // Info of each staker
+    struct StakerInfo {
+        uint256 stakedAmount;      // How many stake tokens the user has provided
+        uint256 lastUpdatedBlock;  // Last block number that user behavior occurs
+        uint256 accInterest;       // Accumulated interest the user has owned
+        bool inBlackList;          // Is staker in blackList
     }
 
+    // The STAKED TOKEN
     address public immutable STAKED_TOKEN;
+    // The REWARD TOKEN
     address public immutable REWARD_TOKEN;
+    // The block when stake starts
     uint256 public immutable START_BLOCK;
+    // The block when stake ends
     uint256 public immutable END_BLOCK;
-    uint256 private _total_interest;
-    uint256 private _total_reward;
+    // The total interest of whole stake
+    uint256 public totalInterest;
+    // The total reward amount of whole stake
+    uint256 public totalRewardAmount;
 
-    mapping(address => StakeData) private StakeDatas;
+    // Info of each staker that stakes token
+    mapping(address => StakerInfo) private _stakerInfos;
+
+    event Staked(address indexed user, uint256 amount, uint256 interest);
+    event Redeem(address indexed user, uint256 redeemAmount, uint256 interest);
+    event RewardsClaimed(address indexed to, uint256 amount);
 
     constructor(
-        address stake_token,
-        address reward_token,
-        uint256 start_block,
-        uint256 end_block
+        address _stakeToken,
+        address _rewardToken,
+        uint256 _startBlock,
+        uint256 _endBlock
     ) {
-        require (start_block >= block.number, 'startBlock less than blockNumber');
-        require (end_block > start_block, 'startBlock less than or equal to endBlock');
+        require(_startBlock >= block.number, 'INVALID_START_BLOCK');
+        require(_endBlock > _startBlock, 'INVALID_STAKE_PERIOD');
 
         _initializeAdmin(msg.sender);
-        STAKED_TOKEN = stake_token;
-        REWARD_TOKEN = reward_token;
-        START_BLOCK = start_block;
-        END_BLOCK = end_block;
-        _total_reward = IERC20(reward_token).balanceOf(address(this));
+        STAKED_TOKEN = _stakeToken;
+        REWARD_TOKEN = _rewardToken;
+        START_BLOCK = _startBlock;
+        END_BLOCK = _endBlock;
+        totalRewardAmount = IERC20(_rewardToken).balanceOf(address(this));
     }
 
-    function syncReward() public {
-        require(block.number < END_BLOCK, "over stake period");
-        _total_reward = IERC20(REWARD_TOKEN).balanceOf(address(this));
+    modifier inStakePeriod() {
+        require(block.number < END_BLOCK, "OVER_PERIOD");
+        _;
     }
 
-    function stake(uint256 amount) public returns (uint256 added_interest) {
-        require(block.number < END_BLOCK, "over stake period");
-        require(amount > 0, "cannot stake 0 amount");
+    /**
+     * @dev Updates total reward amount by admin
+     **/
+    function syncReward() public onlyAdmin inStakePeriod {
+        totalRewardAmount = IERC20(REWARD_TOKEN).balanceOf(address(this));
+    }
+
+    function setBlackList(address blacklistAddress) public onlyAdmin {
+        _stakerInfos[blacklistAddress].inBlackList = true;
+    }
+
+    function removeBlackList(address blacklistAddress) public onlyAdmin {
+        _stakerInfos[blacklistAddress].inBlackList = false;
+    }
+
+    /**
+     * @dev Stakes tokens
+     * @param amount Amount to stake
+     **/
+    function stake(uint256 amount) public inStakePeriod returns (uint256 addedInterest) {
+        require(amount > 0, 'INVALID_ZERO_AMOUNT');
 
         Helper.safeTransferFrom(
             STAKED_TOKEN,
@@ -60,54 +91,77 @@ contract Stake is Pausable, ReentrancyGuard, AdminUpgradeable {
             amount
         );
 
-        StakeData memory data = StakeDatas[msg.sender];
+        StakerInfo memory stakerInfo = _stakerInfos[msg.sender];
+        require(!stakerInfo.inBlackList, 'IN_BLACK_LIST');
 
-        data.block_number = data.block_number < START_BLOCK
+        stakerInfo.lastUpdatedBlock = stakerInfo.lastUpdatedBlock < START_BLOCK
             ? START_BLOCK
             : block.number;
 
-        added_interest = amount.mul(END_BLOCK - data.block_number);
+        addedInterest = amount.mul(END_BLOCK - stakerInfo.lastUpdatedBlock);
 
-        _total_interest = _total_interest.add(added_interest);
+        totalInterest = totalInterest.add(addedInterest);
 
-        data.stake_amount = data.stake_amount.add(amount);
-        data.interest = data.interest.add(added_interest);
-        StakeDatas[msg.sender] = data;
+        stakerInfo.stakedAmount = stakerInfo.stakedAmount.add(amount);
+        stakerInfo.accInterest = stakerInfo.accInterest.add(addedInterest);
+
+        emit Staked(msg.sender, amount, addedInterest);
     }
 
-    function redeem(uint256 amount) public returns (uint256 removed_interest) {
-        require(amount > 0, "cannot redeem 0 amount");
-        require(block.number > START_BLOCK, "stake not start");
+    /**
+     * @dev Redeems staked tokens
+     * @param amount Amount to redeem
+     **/
+    function redeem(uint256 amount) public returns (uint256 removedInterest) {
+        require(amount > 0, 'INVALID_ZERO_AMOUNT');
+        require(block.number > START_BLOCK, "STAKE_NOT_STARTED");
 
-        StakeData memory data = StakeDatas[msg.sender];
-        require(data.stake_amount <= amount, "insufficent stake amount");
+        StakerInfo memory stakerInfo = _stakerInfos[msg.sender];
+        require(stakerInfo.stakedAmount <= amount, "INSUFFICIENT_STAKED_AMOUNT");
 
-        data.block_number = block.number < END_BLOCK ? block.number : END_BLOCK;
+        stakerInfo.lastUpdatedBlock = block.number < END_BLOCK ? block.number : END_BLOCK;
 
-        removed_interest = amount.mul(END_BLOCK - data.block_number);
+        removedInterest = amount.mul(END_BLOCK - stakerInfo.lastUpdatedBlock);
 
-        _total_interest = _total_interest.sub(removed_interest);
+        totalInterest = totalInterest.sub(removedInterest);
 
-        data.stake_amount = data.stake_amount.sub(amount);
-        data.interest = data.interest.sub(removed_interest);
-        StakeDatas[msg.sender] = data;
+        stakerInfo.stakedAmount = stakerInfo.stakedAmount.sub(amount);
+        stakerInfo.accInterest = stakerInfo.accInterest.sub(removedInterest);
 
         Helper.safeTransfer(STAKED_TOKEN, msg.sender, amount);
+        emit Redeem(msg.sender, amount, removedInterest);
     }
 
-    function claim() public returns (uint256 claim_reward_amount) {
-        require(block.number > END_BLOCK, "in stake period");
+    /**
+     * @dev Return the total amount of estimated rewards from an staker
+     * @param staker The staker address
+     * @return The rewards
+     */
+    function getEstimatedRewardsBalance(address staker) external view returns (uint256) {
+        StakerInfo memory stakerInfo = _stakerInfos[staker];
+        if (totalInterest != 0) {
+            return totalRewardAmount.mul(stakerInfo.accInterest) / totalInterest;
+        }
+        return 0;
+    }
 
-        StakeData memory data = StakeDatas[msg.sender];
-        require(data.interest > 0, "no interest");
+    /**
+     * @dev Claims all amount of `REWARD_TOKEN` calculated from staker interest
+     **/
+    function claim() public returns (uint256 claimRewardAmount) {
+        require(block.number > END_BLOCK, "STAKE_NOT_FINISHED");
+        require(totalInterest > 0, 'INVALID_ZERO_TOTAL_INTEREST');
 
-        claim_reward_amount =
-            _total_reward.mul(data.interest) /
-            _total_interest;
+        StakerInfo memory stakerInfo = _stakerInfos[msg.sender];
+        require(stakerInfo.accInterest > 0, "INSUFFICIENT_ACCUMULATED_INTEREST");
+        require(!stakerInfo.inBlackList, 'IN_BLACK_LIST');
 
-        data.interest = 0;
-        data.block_number = block.number;
-        StakeDatas[msg.sender] = data;
-        Helper.safeTransfer(REWARD_TOKEN, msg.sender, claim_reward_amount);
+        claimRewardAmount = totalRewardAmount.mul(stakerInfo.accInterest) / totalInterest;
+
+        stakerInfo.accInterest = 0;
+        stakerInfo.lastUpdatedBlock = block.number;
+
+        Helper.safeTransfer(REWARD_TOKEN, msg.sender, claimRewardAmount);
+        emit RewardsClaimed(msg.sender, claimRewardAmount);
     }
 }
