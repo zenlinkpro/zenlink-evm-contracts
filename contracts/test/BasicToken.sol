94f22b003a03cbbd202129c4d700d5c98b4b2c0f
Hayden Fan <haosong0323@gmail.com>
2021-10-20 18:58:11 +0800

    feat(merkle): add MerkleDistributor and generate scripts (#14)
    * feat: add merkleDistributor and generate scripts
    
    * refactor: add `setBalance` function in TestERC20
    
    * test(merkle): add test for merkleDistributor

--- contracts/test/BasicToken.sol
+++ contracts/test/BasicToken.sol
@@ -13,4 +13,15 @@ contract BasicToken is ERC20 {
     ) ERC20(setName, setSymbol) {
         _mint(msg.sender, initialBalance);
     }
+
+    // sets the balance of the address
+    // this mints/burns the amount depending on the current balance
+    function setBalance(address to, uint256 amount) public {
+        uint256 old = balanceOf(to);
+        if (old < amount) {
+            _mint(to, amount - old);
+        } else if (old > amount) {
+            _burn(to, old - amount);
+        }
+    }
 }
