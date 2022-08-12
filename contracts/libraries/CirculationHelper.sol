6f85b21fcf3ef873cf21840b4ac0449460d6f034
Hayden Fan <haosong0323@gmail.com>
2022-08-12 17:23:58 +0800

    feat: init

---
+++ contracts/libraries/CirculationHelper.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { AdminUpgradeable } from "./AdminUpgradeable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract CirculationHelper is AdminUpgradeable {
+    address immutable vxZenlinkToken;
+    address immutable zenlinkToken;
+    address[] lockedContracts;
+
+    uint256 public immutable MIN_PENALTY_RATIO = 5 * 10**16; // 5%
+    uint256 public immutable MAX_PENALTY_RATIO = 15 * 10**16; // 15%
+
+    error ZeroAddress();
+
+    constructor(address _vxZenlinkToken, address _zenlinkToken) {
+        vxZenlinkToken = _vxZenlinkToken;
+        zenlinkToken = _zenlinkToken;
+        _initializeAdmin(msg.sender);
+    }
+
+    function addLockedContract(address lockedContract) external onlyAdmin {
+        if (lockedContract == address(0)) revert ZeroAddress();
+        lockedContracts.push(lockedContract);
+    }
+
+    function removeLockedContract(address lockedContract) external onlyAdmin {
+        if (lockedContract == address(0)) revert ZeroAddress();
+        address[] memory _lockedContracts = lockedContracts;
+        uint256 len = _lockedContracts.length;
+        
+        for (uint256 i = 0; i < len; i++) {
+            if (_lockedContracts[i] == lockedContract) {
+                _lockedContracts[i] = _lockedContracts[len - 1];
+                break;
+            }
+        }
+
+        lockedContracts = _lockedContracts;
+        lockedContracts.pop();
+    }
+
+    function getCirculation() public view returns (uint256 circulation) {
+        circulation = IERC20(zenlinkToken).totalSupply();
+        for (uint256 i = 0; i < lockedContracts.length; i++) {
+            circulation -= IERC20(zenlinkToken).balanceOf(lockedContracts[i]);
+        }
+    }
+
+    function getZenlinkTokenWithdrawFeeRatio() external view returns (uint256 ratio) {
+        uint256 zenlinkCirculation = getCirculation();
+        uint256 x = Math.mulDiv(
+            IERC20(zenlinkToken).balanceOf(vxZenlinkToken),
+            10**18,
+            zenlinkCirculation
+        );
+        ratio = geRatioValue(x);
+    }
+
+    function geRatioValue(uint256 input) public pure returns (uint256) {
+        // y = 15% (x < 0.1)
+        // y = 5% (x > 0.5)
+        // y = 0.175 - 0.25 * x
+        if (input < 10**17) {
+            return MAX_PENALTY_RATIO;
+        } else if (input > 5 * 10**17) {
+            return MIN_PENALTY_RATIO;
+        } else {
+            return 175 * 10**15 - Math.mulDiv(input, 25 * 10**16, 10**18);
+        }
+    }
+}
---
+++ contracts/tokens/vxZenlinkToken.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import { 
+    ERC4626, 
+    IERC20Metadata,
+    ERC20,
+    Math
+} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import { AdminUpgradeable } from "../libraries/AdminUpgradeable.sol";
+import { ICirculationHelper } from "../libraries/interfaces/ICirculationHelper.sol";
+
+contract vxZenlinkToken is ERC4626, AdminUpgradeable {
+    address public circulationHelper;
+
+    constructor(
+        IERC20Metadata _zlk,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) ERC4626(_zlk) {
+        _initializeAdmin(msg.sender);
+    }
+
+    function updateCirculationHelper(address helper) external onlyAdmin {
+        circulationHelper = helper;
+    }
+
+    function previewRedeem(uint256 shares) public view virtual override returns (uint256) {
+        uint256 assets = _convertToAssets(shares, Math.Rounding.Down);
+        uint256 feeRatio = 
+            ICirculationHelper(circulationHelper).getZenlinkTokenWithdrawFeeRatio();
+        uint256 withdrawFeeAmount = Math.mulDiv(assets, feeRatio, 10**18);
+        return assets - withdrawFeeAmount;
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual override returns (uint256) {
+        require(assets <= maxWithdraw(owner), "ERC4626: withdraw more than max");
+
+        uint256 shares = previewWithdraw(assets);
+        uint256 feeRatio = 
+            ICirculationHelper(circulationHelper).getZenlinkTokenWithdrawFeeRatio();
+        uint256 withdrawFeeAmount = Math.mulDiv(assets, feeRatio, 10**18);
+        _withdraw(_msgSender(), receiver, owner, assets - withdrawFeeAmount, shares);
+
+        return shares;
+    }
+}
