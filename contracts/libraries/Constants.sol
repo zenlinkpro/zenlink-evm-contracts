7f68b572fc882ec0989ce64fccac273adf1fec94
Hayden Fan <haosong0323@gmail.com>
2023-03-29 14:54:34 +0800

    fix: add missing contracts

--- contracts/libraries/Constants.sol
+++ contracts/libraries/Constants.sol
@@ -4,4 +4,10 @@ pragma solidity >=0.8.0;
 library Constants {
     /// @dev Used as a flag for identifying the transfer of ETH instead of a token
     address internal constant NATIVE_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    /// @dev Used as a impossible pool address
+    address internal constant IMPOSSIBLE_POOL_ADDRESS = 0x0000000000000000000000000000000000000001;
+    /// @dev The minimum value that can be returned from #getSqrtRatioAtTick. Equivalent to getSqrtRatioAtTick(MIN_TICK)
+    uint160 internal constant MIN_SQRT_RATIO = 4295128739;
+    /// @dev The maximum value that can be returned from #getSqrtRatioAtTick. Equivalent to getSqrtRatioAtTick(MAX_TICK)
+    uint160 internal constant MAX_SQRT_RATIO = 1461446703485210103287273052203988822378723970342;
 }
--- contracts/universal-router/interfaces/uniswap/v3/IUniswapV3Pool.sol
+++ contracts/universal-router/interfaces/uniswap/v3/IUniswapV3Pool.sol
@@ -3,12 +3,17 @@ pragma solidity >=0.8.0;
 
 import {IUniswapV3PoolImmutables} from "./IUniswapV3PoolImmutables.sol";
 import {IUniswapV3PoolState} from "./IUniswapV3PoolState.sol";
+import {IUniswapV3PoolActions} from "./IUniswapV3PoolActions.sol";
 
 /// @title The interface for a Uniswap V3 Pool
 /// @notice A Uniswap pool facilitates swapping and automated market making between any two assets that strictly conform
 /// to the ERC20 specification
 /// @dev The pool interface is broken up into many smaller pieces
 // solhint-disable-next-line no-empty-blocks
-interface IUniswapV3Pool is IUniswapV3PoolImmutables, IUniswapV3PoolState {
+interface IUniswapV3Pool is 
+  IUniswapV3PoolImmutables, 
+  IUniswapV3PoolState,
+  IUniswapV3PoolActions
+{
     
 }
